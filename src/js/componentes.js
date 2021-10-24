c28fc8f948c9037ae854d926363e09566e2714b7
Guillermo Pagan <gpagan@senilix.com>
2021-10-24 08:44:30 -0600

    Segundo commit

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -2,13 +2,13 @@ import '../css/componentes.css';
 
 
 
-export const saludar = ( nombre ) => {
+// export const saludar = ( nombre ) => {
 
-    console.log('Creando etiqueta h1, en el HTML!');
+//     console.log('Creando etiqueta h1, en el HTML!');
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${ nombre }`;
+//     const h1 = document.createElement('h1');
+//     h1.innerText = `Hola, ${ nombre }`;
 
-    document.body.append( h1 );
+//     document.body.append( h1 );
 
-}
+// }
